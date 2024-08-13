9115d291c2269c7c1fd49affcecb9c3a810e0a0c
vini <frato.vini@gmail.com>
2024-08-12 22:26:52 -0300

    fixed vector bug, added stack bu still requires testing

--- algs/data_structures.c
+++ algs/data_structures.c
@@ -3,6 +3,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+/*
+/////////////////////////////////////
+            Vetor amortizado
+////////////////////////////////////
+*/
 
 vector_int construct_vec_i(int size){
     vector_int vec;
@@ -11,6 +18,14 @@ vector_int construct_vec_i(int size){
         vec.elem = NULL;
         vec._private_end_ptr = NULL;
         vec._private_mem_end_ptr = NULL;
+        return vec;
+    }
+
+    if(size == 0){
+        vec.elem = NULL;
+        vec._private_end_ptr = NULL;
+        vec._private_mem_end_ptr = NULL;
+
         return vec;
     }
 
@@ -35,11 +50,11 @@ void init_vec_i(vector_int *vec, int val){
 }
 
 int size_vec_i(vector_int *vec){
-    return (int)((vec->_private_end_ptr - 1) - vec->elem);
+    return (int)((vec->_private_end_ptr) - vec->elem);
 }
 
 int capacity_vec_i(vector_int *vec){
-    return (int)((vec->_private_mem_end_ptr - 1) - vec->elem);
+    return (int)((vec->_private_mem_end_ptr) - vec->elem);
 }
 
 void destroy_vec_i(vector_int *vec){
@@ -60,33 +75,40 @@ void resize_vec_i(vector_int *vec, int val){
 int push_back_vec_i(int val, vector_int *vec){
     if(vec->_private_end_ptr - vec->elem == 0){
         vec->elem = (int *)malloc(sizeof(int));
+
+        if(vec->elem == NULL){
+            printf("\n\nERRO: ALOCACAO DE MEMORIA FALHOU\n\n");
+            return 1;
+        }
+
         vec->_private_end_ptr = vec->elem + 1;
         vec->_private_mem_end_ptr = vec->elem + 1;
 
-        if(vec->elem == NULL){
-            printf("\n\nERRO: ALOCACAO DE MEMORIA FALHOU\n\n");
-            return 1;
-        }
-
         vec->elem[0] = val;
+
+        return 0;
     }else if(vec->_private_end_ptr >= vec->_private_mem_end_ptr){
-        ++vec->_private_end_ptr;
-        vec->_private_mem_end_ptr= vec->elem + 2 * (vec->_private_end_ptr - vec->elem);
-        vec->elem = realloc(vec->elem, (vec->_private_mem_end_ptr - vec->elem) * sizeof(int));
+        int size = (int)(vec->_private_end_ptr - vec->elem);
+        int capacity = (int)(vec->_private_mem_end_ptr - vec->elem) * 2;
+        vec->elem = realloc(vec->elem, capacity * sizeof(int));
 
         if(vec->elem == NULL){
-            printf("\n\nERRO: ALOCACAO DE MEMORIA FALHOU\n\n");
-            return 1;
+            printf("\n\nERRO: RELOCACAO DE MEMORIA FALHOU\n\n");
+            return 2;
         }
- 
-        *(vec->_private_end_ptr-1) = val;
-        ++vec->_private_end_ptr;
-    }else{
-        *(vec->_private_end_ptr-1) = val;
-        ++vec->_private_end_ptr;
-    }
 
-    return 0;
+        vec->_private_end_ptr = vec->elem + size;
+        vec->_private_mem_end_ptr = vec->elem + capacity;
+        *vec->_private_end_ptr = val;
+        ++vec->_private_end_ptr;
+
+        return 0;
+    }else{
+        *vec->_private_end_ptr = val;
+        ++vec->_private_end_ptr;
+
+        return 0;
+    }
 }
 
 void pop_back_vec_i(vector_int *vec){
@@ -111,7 +133,7 @@ int erase_vec_i(vector_int *vec, int* element){
     int capacity = (int)(vec->_private_mem_end_ptr - vec->elem);    
     int *tmp = (int *)malloc( capacity * sizeof(int));
     if(tmp == NULL){
-        printf("\n\nERRO: ALOCACAO DE MEMORIA FALHOU\n\n");
+        printf("\n\nERRO:: ALOCACAO DE MEMORIA FALHOU\n\n");
         return 1;
     }
 
@@ -130,4 +152,108 @@ int erase_vec_i(vector_int *vec, int* element){
     vec->_private_end_ptr = vec->elem + size-1;
     vec->_private_mem_end_ptr = vec->elem + capacity;
     return 0;
+}
+
+/*
+/////////////////////////////////////////////////////
+                    Stack
+/////////////////////////////////////////////////////
+*/
+
+struct data_structure_stack_int{
+    int *start;
+    int *end;
+    int *capacity;
+};
+
+stack_int* construct_stack_i() {
+    stack_int *tmp = (stack_int *)malloc(sizeof(stack_int));
+    tmp->start = NULL;
+    tmp->end = NULL;
+    tmp->capacity = NULL;
+
+    return tmp;
+}
+
+void destroy_stack_i(stack_int *stck) {
+    if(stck->start == NULL) return;
+
+    free(stck->start);
+}
+
+int get_top_stack_i(stack_int *stck) {
+    if(stck->start == stck->end){
+        printf("\n\nERRO:: SEM VALOR NO TOPO PARA RETORNAR\n\n");
+        return -451;
+    }
+
+    return *--stck->end;
+}
+
+int set_top_stack_i(int val, stack_int *stck) {
+    if(stck->start == stck->end) {
+        printf("\n\nERRO:: SEM VALOR NO TOPO PARA RETORNAR\n\n");
+        return 1;
+    }
+
+    *--stck->end = val;
+    return 0;
+}
+
+int push_back_stack_i(int val, stack_int *stck) {
+    if(stck->start == stck->end) {
+        stck->start = (int *)malloc(sizeof(int));
+
+        if (stck->start == NULL) {
+            printf("\n\nERRO:: ALOCACAO DE MEMORIA FALHOU\n\n");
+            return 1;
+        }
+
+        stck->end = stck->start + 1;
+        stck->capacity = stck->start +1;
+        stck->start[0] = val;
+
+        return 0;
+    }else if(stck->end >= stck->capacity) {
+        int size = (int)(stck->end - stck->start);
+        int capacity = (int)(stck->capacity - stck->start) * 2;
+
+        stck->start = realloc(stck->start, capacity * sizeof(int));
+
+        if(stck->start == NULL) {
+            printf("\n\nERRO:: REALOCACAO DE MEMORIA FALHOU\n\n");
+            return 2;
+        }
+
+        stck->end = stck->start + size;
+        stck->end = stck->start + capacity;
+
+        *stck->end = val;
+        ++stck->end;
+
+        return 0;
+    }else{
+        *stck->end = val;
+        ++stck->end;
+
+        return 0;
+    }
+}
+
+int pop_back_stack_i(stack_int *stck) {
+    if(stck->end - stck->start) {
+        --stck->end;
+        return *(stck->end + 1);
+    }else {
+        printf("ERRO:: NAO HA ELEMENTOS PARA REMOVER");
+        return -451;
+    }
+}
+
+int size_stack_i(stack_int *stck){
+    return (int)(stck->end - stck->start);
+}
+
+bool is_empty_stack_i(stack_int *stck){
+    return stck->start == stck->end ? true:false;
 }
--- algs/data_structures.h
+++ algs/data_structures.h
@@ -1,20 +1,68 @@
 #pragma once
 #include <stdbool.h>
+/*
+    Se deseja usar void * só mudar para void * na sua implementação, se quiser
+ser mais correto ainda use macros eg: max e min da stdlib, analogamente se
+quiser que o tipo seja float ou etc só trocar o tipo, se pa seja até melhor
+porque você re-olha a implementação pra ela ficar mais na sua cabeça.
+*/
 
-typedef struct data_structure_vector_int{
+/*
+    Estrutura de dado similar ao do STL vector do c++ não segue as regras do rudinei
+por não ser completamente privada. Entretanto por causa disso acaba sendo mais
+segura pois evita do usúario de utilizar um pointeiro para o dado, de forma a ter
+acesso facil, e depois alguma das funções mudar esse pointeiro e der algum erro.
+*/
+
+typedef struct data_structure_vector_int {
     int *elem;
     int *_private_end_ptr;
     int *_private_mem_end_ptr;
 }vector_int;
 
-vector_int construct_vec_i(int size);
-void init_vec_i(vector_int *vec, int val);
-void destroy_vec_i(vector_int *vec);
-int size_vec_i(vector_int *vec);
-int capacity_vec_i(vector_int *vec);
-bool is_empty_vec_i(vector_int *vec);
-void resize_vec_i(vector_int *vec, int val);
-int push_back_vec_i(int val, vector_int *vec);
-void pop_back_vec_i(vector_int *vec);
-int shrink_to_fit_vec_i(vector_int *vec);
-int erase_vec_i(vector_int *vec, int* element);
+vector_int construct_vec_i (int size);
+void init_vec_i (vector_int *vec, int val);
+void destroy_vec_i (vector_int *vec);
+int size_vec_i (vector_int *vec);
+int capacity_vec_i (vector_int *vec);
+bool is_empty_vec_i (vector_int *vec);
+void resize_vec_i (vector_int *vec, int val);
+
+//Retorna 0 se foi efetuado com sucesso e differente de 0 caso teve algum erro
+int push_back_vec_i (int val, vector_int *vec);
+void pop_back_vec_i (vector_int *vec);
+int shrink_to_fit_vec_i (vector_int *vec);
+int erase_vec_i (vector_int *vec, int* element);
+
+/*
+    Stack, também chamda de pilha, é uma estrutura que tem o intuito de ter accesos
+e modificações apenas através de seu topo, seguindo a lógica do LIFO. 
+    Nesta implementação utiliza um vetor armortizdo como base, entrtanto ele pode 
+utilizar de  outros tipos de containers para implementar sua função.
+*/
+
+typedef struct data_structure_stack_int stack_int;
+
+//Construtor padrão de uma stack
+stack_int* construct_stack_i();
+
+//deleta todos os elementos da stack
+void destroy_stack_i(stack_int *stck);
+
+//retorna o valor do topo da stack   
+int get_top_stack_i(stack_int *stck);
+
+//modifica o valor do topo da stack
+int set_top_stack_i(int val, stack_int* stck);
+
+//Retorna 0 se foi efetuado com sucesso e 1 caso teve algum erro
+int push_back_stack_i (int val, stack_int *stck);
+
+//Remove o ultimo item da stack e retorna seu valor
+int pop_back_stack_i (stack_int *stck);
+
+//Retorna o tamanho da stack
+int size_stack_i(stack_int *stck);
+
+//Checa se a stack esta vazia
+bool is_empty_stack_i (stack_int *stck);
